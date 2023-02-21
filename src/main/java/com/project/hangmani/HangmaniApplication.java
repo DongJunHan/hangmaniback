4f6e9fe368a1802f58210e3fce040baf048b5fd5
DongJunHan <gamedokdok@naver.com>
2023-02-21 21:01:39 +0900

    initial commit

---
+++ src/main/java/com/project/hangmani/HangmaniApplication.java
@@ -0,0 +1,13 @@
+package com.project.hangmani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmaniApplication.class, args);
+	}
+
+}
