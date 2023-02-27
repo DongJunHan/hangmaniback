8855d582830775d553131d8a7888b9cee3078f6a
DongJunHan <gamedokdok@naver.com>
2023-02-27 20:22:50 +0900

    repository change

--- src/main/java/com/project/hangmani/model/management/BoardRepository.java
+++ src/main/java/com/project/hangmani/model/management/BoardRepository.java
@@ -1,7 +1,14 @@
 package com.project.hangmani.model.management;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
 
 @Repository
 public class BoardRepository {
+    private JdbcTemplate template;
+    public BoardRepository(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
+    }
 }
