4090ea613ea5800fab4dceb51ba1951cdd69b191
DongJunHan <gamedokdok@naver.com>
2023-03-13 23:08:25 +0900

    json 형태의 http response를 hashmap으로 바꾸는 함수 추가

--- src/main/java/com/project/hangmani/util/ConvertData.java
+++ src/main/java/com/project/hangmani/util/ConvertData.java
@@ -1,24 +1,38 @@
 package com.project.hangmani.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Map;
 
 public class ConvertData {
-    private ObjectMapper objectMapper = new ObjectMapper();
-    public String DAOToJson(Object dto){
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public String DAOToJson(Object dto) {
         try {
-            String result = objectMapper.writeValueAsString(dto);
-            return result;
+            return objectMapper.writeValueAsString(dto);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public Map<String, String> JsonToMap(String data) throws JsonProcessingException {
+        return objectMapper.readValue(data, new TypeReference<>() {});
+
+    }
+
     public java.sql.Date getSqlDate() {
         Date date = new Date();
         long time = date.getTime();
         return new java.sql.Date(time);
+    }
+
+    public java.sql.Date convertSqlDate(long timeStamp) {
+        Timestamp timestamp = new Timestamp(timeStamp * 1000);
+        return new java.sql.Date(timestamp.getTime());
     }
 
 
