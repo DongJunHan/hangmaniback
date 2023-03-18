2d4c863497ffae31abcacb83ec402afc2395b634
DongJunHan <gamedokdok@naver.com>
2023-03-18 18:05:29 +0900

    1. webclient를 전역변수로 di받도록 수정. 2. 카카오 회원 정보 받아오는 로직 (아직 성공 못함)

--- src/main/java/com/project/hangmani/service/OAuthService.java
+++ src/main/java/com/project/hangmani/service/OAuthService.java
@@ -1,15 +1,13 @@
 package com.project.hangmani.service;
 
-import com.project.hangmani.domain.User;
+import com.project.hangmani.exception.KO310Exception;
 import com.project.hangmani.repository.UserRepository;
 import com.project.hangmani.util.ConvertData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.sql.Date;
 import java.util.Map;
 
 import static com.project.hangmani.config.OAuthConst.*;
@@ -19,42 +17,60 @@ import static com.project.hangmani.config.OAuthConst.*;
 @Slf4j
 public class OAuthService {
     private UserRepository userRepository;
+    private WebClient webClient;
     private ConvertData convertData;
 
-    public OAuthService(UserRepository userRepository) {
+    public OAuthService(UserRepository userRepository, WebClient webClient) {
         this.userRepository = userRepository;
+        this.webClient = webClient;
         this.convertData = new ConvertData();
     }
 
-    public String redirectPostKakao(String code) {
+    public Map<String, String> getAccessToken(String code) {
 //        response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}
-        WebClient client = WebClient.create();
-        String response = client.post()
+
+        String responseBody = webClient.post()
                 .uri(builder -> builder.scheme("https")
                         .host(HOST)
                         .path(URI)
-                        .queryParam("response_type","code")
-//                        .queryParam("grant_type",GRANT_TYPE)
+                        .queryParam("grant_type", GRANT_TYPE)
                         .queryParam("client_id", CLIENT_ID)
-                        .queryParam("redirect_uri",POST_REDIRECT_URI)
-//                        .queryParam("code", code)
-//                        .queryParam("client_secret","")
+                        .queryParam("redirect_uri", POST_REDIRECT_URI)
+                        .queryParam("code", code)
+                        .queryParam("client_secret", "")
                         .build())
                 .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
                 .syncBody("")
-                .retrieve()
+                .exchange()
+                .block()
                 .bodyToMono(String.class)
                 .block();
-        log.info("response={}", response);
-//        Map<String, String> respTable = convertData.JsonToMap(response);
-//        log.info("table={}",respTable);
-        return "basic/login";
+        log.info("response={}", responseBody);
+        Map<String, String> respTable = convertData.JsonToMap(responseBody);
+        if (respTable.containsKey("error")) {
+            if (respTable.get("error_code") == "KOE310"){
+                throw new KO310Exception(respTable.get("error_description"));
+            } else if (respTable.get("error_code") == "KOE303") {
+                throw new KO310Exception(respTable.get("error_description"));
+            }
+        }
+
+        return respTable;
     }
 
-//    private User InsertUser(String token, String refreshToken, int tokenExpire, int refreshTokenExpire, String option){
-//        Date convertTokenExpire = convertData.convertSqlDate((long) tokenExpire);
-//        Date convertRefreshTokenExpire = convertData.convertSqlDate((long) refreshTokenExpire);
-//        int ret = userRepository.insertUser(token, refreshToken, convertTokenExpire, convertRefreshTokenExpire, option);
-//
-//    }
+    public String getKakaoUserInfo(Map<String, String> tokenInfos) {
+        log.info("access_token={}",tokenInfos.get("access_token"));
+        String responseBody = webClient.get()
+                .uri(builder -> builder.scheme("https")
+                        .host(HOST)
+                        .path(USER_INFO_URI)
+                        .build())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenInfos.get("access_token"))
+                .exchange()
+                .block()
+                .bodyToMono(String.class)
+                .block();
+        log.info("response={}",responseBody);
+        return responseBody;
+    }
 }
