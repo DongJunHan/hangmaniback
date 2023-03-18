23aba2cb7b0a10846f4df11729a5a8594b635a79
DongJunHan <gamedokdok@naver.com>
2023-03-18 18:03:55 +0900

    OAuthConst 회정정보 받아오는 uri 선언, 추가동의항목 받아오는 uri삭제

--- src/main/java/com/project/hangmani/config/OAuthConst.java
+++ src/main/java/com/project/hangmani/config/OAuthConst.java
@@ -3,7 +3,7 @@ package com.project.hangmani.config;
 public class OAuthConst {
     public static final String CLIENT_ID = "eec30559b1482179d83dd69beb47a816";
     public static final String URI = "/oauth/token";
-    public static final String ACCESS_URI = "/oauth/token/authorize";
+    public static final String USER_INFO_URI = "/v2/user/me";
     public static final String HOST = "kauth.kakao.com";
     public static final String POST_REDIRECT_URI = "http://localhost:8080/user";
     public static final String GRANT_TYPE = "authorization_code";
