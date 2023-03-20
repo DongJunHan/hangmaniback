bcb47918c237ecdcb974f37cd1757da84ff55f30
DongJunHan <gamedokdok@naver.com>
2023-03-20 14:27:04 +0900

    AES 암호 알고리즘 추가.

---
+++ src/main/java/com/project/hangmani/security/AES.java
@@ -0,0 +1,90 @@
+package com.project.hangmani.security;
+
+import com.project.hangmani.exception.InvalidKeySpec;
+import com.project.hangmani.exception.NoSuchAlgorithm;
+import com.project.hangmani.exception.NullPointKey;
+import com.project.hangmani.util.Util;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+
+import static com.project.hangmani.config.SecurityConst.*;
+
+@Slf4j
+public class AES {
+    private byte[] key;
+
+    @PostConstruct
+    public void init() {
+        Util util = new Util();
+
+        this.key = util.getKeyFromFile();
+        if(this.key == null) {
+            this.key = createAESKey(KEY_SIZE);
+            util.saveByteData(this.key);
+        }
+    }
+
+
+    public byte[] encryptData(String text) {
+        Cipher cipher = getCipher();
+        // Encrypt plaintext
+        try {
+            return cipher.doFinal(text.getBytes());
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public byte[] decryptData(byte[] encryptData) {
+        Cipher cipher = getCipher();
+        try {
+            return cipher.doFinal(encryptData);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Cipher getCipher() {
+        if (key == null && key.length == 0)
+            throw new NullPointKey();
+        // Create secret key
+        SecretKeySpec secretKeySpec = new SecretKeySpec(this.key, ALGORITHM);
+        // Create cipher
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            return cipher;
+        } catch (NoSuchAlgorithmException e) {
+            throw new NoSuchAlgorithm("getCipher", e);
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new InvalidKeySpec("getCipher",e);
+        }
+    }
+
+    private byte[] createAESKey(int keySize) {
+        if (keySize != 128 && keySize != 192 && keySize != 256) {
+            throw new IllegalArgumentException("Key size must be 128, 192, or 256 bits");
+        }
+        KeyGenerator keyGenerator;
+        try {
+            keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        keyGenerator.init(keySize);
+        SecretKey secretKey = keyGenerator.generateKey();
+        return secretKey.getEncoded();
+    }
+}
+
