c64a2894d26c47f03e692812b17aea7dd2801a58
DongJunHan <gamedokdok@naver.com>
2023-03-20 14:47:16 +0900

    oauth 고정 값 설정

--- src/main/java/com/project/hangmani/config/OAuthConst.java
+++ src/main/java/com/project/hangmani/config/OAuthConst.java
@@ -5,6 +5,7 @@ public class OAuthConst {
     public static final String URI = "/oauth/token";
     public static final String USER_INFO_URI = "/v2/user/me";
     public static final String HOST = "kauth.kakao.com";
+    public static final String USER_INFO_HOST = "kapi.kakao.com";
     public static final String POST_REDIRECT_URI = "http://localhost:8080/user";
     public static final String GRANT_TYPE = "authorization_code";
 
