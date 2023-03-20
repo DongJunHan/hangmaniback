fe2f93e79d08cca2ba24a9076d82db33158a36d2
DongJunHan <gamedokdok@naver.com>
2023-03-20 14:45:20 +0900

    암호로직 수행시 고정 값 설정

---
+++ src/main/java/com/project/hangmani/config/SecurityConst.java
@@ -0,0 +1,8 @@
+package com.project.hangmani.config;
+
+public class SecurityConst {
+    public static final String KEY_FILE_PATH = "./config";
+    public static final String ALGORITHM = "AES";
+    public static final String CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+    public static final int KEY_SIZE = 256;
+}
