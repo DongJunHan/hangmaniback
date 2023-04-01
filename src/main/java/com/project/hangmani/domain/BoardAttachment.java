c7e6d912da1e86e6902b0a36a0f2d58b3cf8e168
DongJunHan <gamedokdok@naver.com>
2023-04-01 20:34:00 +0900

    게시물 첨부파일 Entity 변경

--- src/main/java/com/project/hangmani/domain/BoardAttachment.java
+++ src/main/java/com/project/hangmani/domain/BoardAttachment.java
@@ -1,9 +1,6 @@
 package com.project.hangmani.domain;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.sql.Date;
 
@@ -11,26 +8,14 @@ import java.sql.Date;
 @Setter
 @ToString
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardAttachment {
     private int attachmentNo;
-    private int reviewNo;
     private String originalFileName;
-    private String renamedFileName;
+    private String savedFileName;
+    private long fileSize;
     private Date uploadDate;
-    private int downloadCount ;
-    private String status; //첨부파일 삭제여부
+    private Byte[] fileData;
 
-    public BoardAttachment() {
-    }
-
-    public BoardAttachment(int attachmentNo, int reviewNo, String originalFileName,
-                           String renamedFileName, Date uploadDate, int downloadCount, String status) {
-        this.attachmentNo = attachmentNo;
-        this.reviewNo = reviewNo;
-        this.originalFileName = originalFileName;
-        this.renamedFileName = renamedFileName;
-        this.uploadDate = uploadDate;
-        this.downloadCount = downloadCount;
-        this.status = status;
-    }
 }
