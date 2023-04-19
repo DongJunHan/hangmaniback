383fca4fffc8cf25aec526f6d52722945d79ff9a
DongJunHan <gamedokdok@naver.com>
2023-04-19 20:32:38 +0900

    Assertion으로 유효성체크

--- src/test/java/com/project/hangmani/repository/FileRepositoryTest.java
+++ src/test/java/com/project/hangmani/repository/FileRepositoryTest.java
@@ -2,9 +2,9 @@ package com.project.hangmani.repository;
 
 import com.project.hangmani.domain.StoreAttachment;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -17,12 +17,17 @@ class FileRepositoryTest {
     FileRepository fileRepository;
     @Test
     void insertAttachment() {
+
     }
     @Test
     void getAttachmentsByUuid() {
         String storeUuid = "8c33e236-ac2c-11ed-9b15-12ebd169e012";
         List<StoreAttachment> attachmentsByUuid = fileRepository.getAttachmentsByUuid(storeUuid);
         log.info("attach={}", attachmentsByUuid);
+        for (StoreAttachment st:attachmentsByUuid
+             ) {
+            Assertions.assertThat(storeUuid).isEqualTo(st.getStoreUuid());
+        }
 
     }
 }
