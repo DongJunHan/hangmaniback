298e913f8034b83d88cdffa02738898450d5c9a0
DongJunHan <gamedokdok@naver.com>
2023-04-29 22:23:39 +0900

    h2데이터베이스 외에는 해당 user defined function을 등록하지 않도록 로직 수정

--- src/main/java/com/project/hangmani/connection/CustomFunctionConfig.java
+++ src/main/java/com/project/hangmani/connection/CustomFunctionConfig.java
@@ -1,25 +1,33 @@
 package com.project.hangmani.connection;
 
 import com.project.hangmani.util.Util;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class CustomFunctionConfig {
     private final JdbcTemplate jdbcTemplate;
+    private String dbName;
 
 
-    public CustomFunctionConfig(JdbcTemplate jdbcTemplate) {
+    public CustomFunctionConfig(JdbcTemplate jdbcTemplate, @Value(value = "${spring.datasource.driver-class-name}") String dbName) {
+        this.dbName = dbName;
         this.jdbcTemplate = jdbcTemplate;
     }
-
+    @PostConstruct
     public void registerFunctions() {
-        String packageName = Util.class.getPackage().getName();
-        String className = Util.class.getSimpleName();
-        String qualifiedClassName = packageName + "." + className;
+        if (null != dbName && dbName.contains("h2")) {
+            String packageName = Util.class.getPackage().getName();
+            String className = Util.class.getSimpleName();
+            String qualifiedClassName = packageName + "." + className;
 //        Delete alias
-        this.jdbcTemplate.execute("DROP ALIAS IF EXISTS get_distance;");
-        this.jdbcTemplate.execute("CREATE ALIAS IF NOT EXISTS get_distance  FOR \""+qualifiedClassName+".getDistance\";");
+            this.jdbcTemplate.execute("DROP ALIAS IF EXISTS get_distance;");
+            this.jdbcTemplate.execute("CREATE ALIAS IF NOT EXISTS get_distance  FOR \"" + qualifiedClassName + ".getDistance\";");
+        }
     }
 
 }
