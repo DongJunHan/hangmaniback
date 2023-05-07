fa0a2904a8e744f1e1b30c6640c576a51a77b58e
DongJunHan <gamedokdok@naver.com>
2023-05-07 22:55:47 +0900

    ㅋㅓ밋

--- src/test/java/com/project/hangmani/service/BoardServiceTest.java
+++ src/test/java/com/project/hangmani/service/BoardServiceTest.java
@@ -1,6 +1,6 @@
 package com.project.hangmani.service;
 
-import com.project.hangmani.dto.BoardDTO.RequestBoardDTO;
+import com.project.hangmani.dto.BoardDTO.RequestBoardInsertDTO;
 import com.project.hangmani.dto.BoardDTO.ResponseBoardDTO;
 import com.project.hangmani.exception.NotFoundUser;
 import com.project.hangmani.repository.BoardRepository;
@@ -8,8 +8,6 @@ import com.project.hangmani.repository.UserRepository;
 import com.project.hangmani.security.AES;
 import com.project.hangmani.util.Util;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,7 +59,7 @@ class BoardServiceTest {
     @Test
     @DisplayName("H2 데이터베이스 게시물 삽입 성공")
     void BoardInsertTestSuccess() {
-        RequestBoardDTO requestBoardDTO = new RequestBoardDTO("공지사항12", "안녕하십니까 공지사항입니다^& </li>", "id1");
+        RequestBoardInsertDTO requestBoardDTO = new RequestBoardInsertDTO("공지사항12", "안녕하십니까 공지사항입니다^& </li>", "id1");
         ResponseBoardDTO response = boardService.createBoard(requestBoardDTO);
         log.info("result={}", response);
         assertThat(response.getBoardContent()).isEqualTo(requestBoardDTO.getBoardContent());
@@ -70,7 +68,7 @@ class BoardServiceTest {
     @Test
     @DisplayName("H2 데이터베이스 게시물 존재하지 않는 사용자")
     void BoardInsertTestFail() {
-        RequestBoardDTO requestBoardDTO = new RequestBoardDTO("공지사항12", "안녕하십니까 공지사항입니다^& </li>", "string");
+        RequestBoardInsertDTO requestBoardDTO = new RequestBoardInsertDTO("공지사항12", "안녕하십니까 공지사항입니다^& </li>", "string");
         assertThatThrownBy(() -> boardService.createBoard(requestBoardDTO))
                 .isInstanceOf(NotFoundUser.class);
     }
