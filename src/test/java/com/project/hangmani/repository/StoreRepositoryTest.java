d1d5db0b2f4222d5fe9861ceb2f38c0647bb4538
DongJunHan <gamedokdok@naver.com>
2023-06-11 17:24:35 +0900

    @BeforeAll 어노테이션을 통하여 sql script파일은 한 번만 로드하도록 변경. 단점은 각 테스트마다 영향이 간다는 것에 있다. @BeforeEach로 다시 변경하고, @AfterEach로 drop하는 sql script파일을 수행하는 것으로 변경해야할지도 모름. 장점은 데이터 로드를 한 번만 하기 때문에 테스트 속도가 빨라진다.

--- src/test/java/com/project/hangmani/repository/StoreRepositoryTest.java
+++ src/test/java/com/project/hangmani/repository/StoreRepositoryTest.java
@@ -1,51 +1,111 @@
 package com.project.hangmani.repository;
 
 import com.project.hangmani.domain.Store;
+import com.project.hangmani.dto.StoreDTO.RequestStoreFilterDTO;
 import com.project.hangmani.util.Util;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
 
-@TestExecutionListeners(SqlScriptsTestExecutionListener.class)
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
 class StoreRepositoryTest {
-    private StoreRepository storeRepository;
-    private DataSource dataSource;
-    private Util utilMock;
+    private static StoreRepository storeRepository;
+    private static DataSource dataSource;
+    private static Util utilMock;
+    private static JdbcTemplate template;
     private String testStoreUuid= "8c31ad72-ac2c-11ed-9b15-12ebd169e012";
-
-    @BeforeEach
-    void setDI() {
+    private String failStoreUuid = "c31ad72-ac2c-11ed-9b15-12ebd169e012";
+    @BeforeAll
+    static void preDB() {
         dataSource = new DriverManagerDataSource("jdbc:h2:mem:test","sa","");
         utilMock = Mockito.mock(Util.class);
         storeRepository = new StoreRepository(dataSource, utilMock);
+
+        template = new JdbcTemplate(dataSource);
+        Resource schema = new ClassPathResource("schema.sql");
+        Resource data = new ClassPathResource("data.sql");
+        try {
+            ScriptUtils.executeSqlScript(template.getDataSource().getConnection(), schema);
+            ScriptUtils.executeSqlScript(template.getDataSource().getConnection(), data);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String packageName = Util.class.getPackage().getName();
+        String className = Util.class.getSimpleName();
+        String qualifiedClassName = packageName + "." + className;
+
+        // 사용자 정의 함수 등록
+        template.execute("CREATE ALIAS IF NOT EXISTS get_distance FOR \"" + qualifiedClassName + ".getDistance\";");
     }
+    @AfterEach
+    void afterDB() {
 
-
+    }
     @Test
     void getStoreInfoByLatitudeLongitude() {
     }
     @Test
-    void testInMemoryH2DB() {
-        TEST test = storeRepository.testInMemoryH2DB(1);
-        Assertions.assertThat(1).isEqualTo(test.getNum());
+    void testGetTableLists() {
+        List<List<String>> tableList = storeRepository.getTableList();
+        for (List<String> t :
+                tableList) {
+            for (String a :
+                    t) {
+                log.info("@@: {}", a);
+            }
+        }
+        Assertions.assertThat(tableList.size()).isPositive();
+
     }
     @Test
     void getStoreInfoByUuid() {
+        //given
+
+        //when
         Store store = storeRepository.getStoreInfoByUuid(testStoreUuid);
+        //then
         Assertions.assertThat(testStoreUuid).isEqualTo(store.getStoreUuid());
-        Assertions.assertThat(store).isInstanceOf(Store.class);
+    }
+    @Test
+    void failStoreInfoByUuid() {
+        assertThrows(EmptyResultDataAccessException.class, () -> storeRepository.getStoreInfoByUuid(failStoreUuid));
     }
 
     @Test
     void getStoreInfoWithWinCountBySidoSigugun() {
+        //given
+        RequestStoreFilterDTO requestDTO = RequestStoreFilterDTO.builder()
+                .sido("인천")
+                .sigugun("부평구")
+                .filter("1st")
+                .lottoID(1)
+                .offset(0)
+                .limit(10)
+                .userLatitude(37.39130205)
+                .userLongitude(127.077625)
+                .build();
+
+        //when
+        List<Store> result = storeRepository.getStoreInfoWithWinCountBySidoSigugun(requestDTO);
+        log.info("result: {}", result);
+        //then
+        Assertions.assertThat(result.size()).isPositive();
     }
 
     @Test
@@ -70,24 +130,5 @@ class StoreRepositoryTest {
 
     @Test
     void insertStoreInfo() {
-    }
-
-    @Profile("test")
-    static class TestCustomFunctionConfig {
-        private final JdbcTemplate jdbcTemplate;
-
-        public TestCustomFunctionConfig(JdbcTemplate jdbcTemplate) {
-            this.jdbcTemplate = jdbcTemplate;
-        }
-
-        @BeforeEach
-        public void registerFunctions() {
-            String packageName = Util.class.getPackage().getName();
-            String className = Util.class.getSimpleName();
-            String qualifiedClassName = packageName + "." + className;
-
-            // 사용자 정의 함수 등록
-            this.jdbcTemplate.execute("CREATE ALIAS IF NOT EXISTS get_distance FOR \"" + qualifiedClassName + ".getDistance\";");
-        }
     }
 }
