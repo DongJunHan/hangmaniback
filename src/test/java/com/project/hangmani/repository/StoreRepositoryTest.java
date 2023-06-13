da6647e76f1b5a4f5131093503d25b3cfd947097
DongJunHan <gamedokdok@naver.com>
2023-06-13 22:51:16 +0900

    1. 위도경도 성공 케이스 추가 2. 상점 삽입 성공 케이스 추가 3. 상점 삽입 실패 케이스 추가

--- src/test/java/com/project/hangmani/repository/StoreRepositoryTest.java
+++ src/test/java/com/project/hangmani/repository/StoreRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.project.hangmani.repository;
 
 import com.project.hangmani.domain.Store;
+import com.project.hangmani.dto.StoreDTO;
 import com.project.hangmani.dto.StoreDTO.RequestStoreFilterDTO;
 import com.project.hangmani.util.Util;
 import lombok.extern.slf4j.Slf4j;
@@ -8,13 +9,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -33,7 +34,7 @@ class StoreRepositoryTest {
     @BeforeAll
     static void preDB() {
         dataSource = new DriverManagerDataSource("jdbc:h2:mem:test","sa","");
-        utilMock = Mockito.mock(Util.class);
+        utilMock = new Util();//Mockito.mock(Util.class);
         storeRepository = new StoreRepository(dataSource, utilMock);
 
         template = new JdbcTemplate(dataSource);
@@ -67,7 +68,7 @@ class StoreRepositoryTest {
                 tableList) {
             for (String a :
                     t) {
-                log.info("@@: {}", a);
+                log.info("value: {}", a);
             }
         }
         Assertions.assertThat(tableList.size()).isPositive();
@@ -129,19 +130,19 @@ class StoreRepositoryTest {
     }
     @Test
     void unNormalOffsetStoreInfoWinCountBySidoSigugun() {
+        int offset = 5;
+        int limit = 1;
         //given
         RequestStoreFilterDTO requestDTO = RequestStoreFilterDTO.builder()
                 .sido("인천")
                 .sigugun("부평구")
                 .filter("1st")
                 .lottoID(1)
-                .offset(5)
-                .limit(10)
+                .offset(offset)
+                .limit(limit)
                 .userLatitude(37.39130205)
                 .userLongitude(127.077625)
                 .build();
-        log.info("offset: {}", requestDTO.getOffset());
-        log.info("limit: {}", requestDTO.getLimit());
         //when
         List<Store> result = storeRepository.getStoreInfoWithWinCountBySidoSigugun(requestDTO);
         log.info("result: {}", result);
@@ -150,7 +151,22 @@ class StoreRepositoryTest {
     }
 
     @Test
-    void getStoreInfoWithWinCountByLatitudeLongitude() {
+    void successStoreInfoWithWinCountByLatitudeLongitude() {
+        //given
+        RequestStoreFilterDTO requestDTO = RequestStoreFilterDTO.builder()//인천 부평구
+                .userLatitude(37.39130205)
+                .userLongitude(127.077625)
+                .lottoID(1)
+                .startLatitude(37.4357)
+                .endLatitude(37.5184)
+                .startLongitude(126.6779)
+                .endLongitude(126.7735)
+                .build();
+        //when
+        List<Store> result = storeRepository.getStoreInfoWithWinCountByLatitudeLongitude(requestDTO);
+
+        //then
+        Assertions.assertThat(result.size()).isPositive();
     }
 
     @Test
@@ -170,6 +186,36 @@ class StoreRepositoryTest {
     }
 
     @Test
-    void insertStoreInfo() {
+    @Transactional
+    void successInsertStoreInfo() {
+        //given
+        StoreDTO.RequestStoreInsertDTO requestDTO = StoreDTO.RequestStoreInsertDTO.builder()
+                .storeName("테스트 상점이름")
+                .storeAddress("서울 강남구 테스트주소")
+                .storeLatitude(37.4921)
+                .storeLongitude(127.0443)
+                .build();
+
+        //when
+        String storeUuid = storeRepository.insertStoreInfo(requestDTO);
+        log.info("uuid={}", storeUuid);
+        Store store = storeRepository.getStoreInfoByUuid(storeUuid);
+        log.info("store={}", store);
+        //then
+        Assertions.assertThat(storeUuid).isNotNull();
+    }
+    @Test
+    void failInsertStoreInfo() {
+        //given
+        StoreDTO.RequestStoreInsertDTO requestDTO = StoreDTO.RequestStoreInsertDTO.builder()
+                .storeName("테스트 상점이름")
+                .storeAddress("")//올바르지 않은 주소
+                .storeLatitude(37.4921)
+                .storeLongitude(127.0443)
+                .build();
+        //when
+        String storeUuid = storeRepository.insertStoreInfo(requestDTO);
+        //then
+        Assertions.assertThat(storeUuid).isNull();
     }
 }
