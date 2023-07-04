7e8cd85e87d2797c488afbdaddcf0964eaf39ef5
DongJunHan <gamedokdok@naver.com>
2023-07-04 19:38:06 +0900

    lotto type domain 추가

---
+++ src/main/java/com/project/hangmani/domain/LottoType.java
@@ -0,0 +1,10 @@
+package com.project.hangmani.domain;
+
+import lombok.Data;
+
+@Data
+public class LottoType {
+    private String storeUuid;
+    private Integer lottoId;
+    private String lottoName;
+}
