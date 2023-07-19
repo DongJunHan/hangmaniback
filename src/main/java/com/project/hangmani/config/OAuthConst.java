8ac02088854f1fab859921af5658f45c9c9fa98d
DongJunHan <gamedokdok@naver.com>
2023-07-20 00:31:34 +0900

    chore: 서버포트가 8080에서 9090으로 변경되어서 수정

--- src/main/java/com/project/hangmani/config/OAuthConst.java
+++ src/main/java/com/project/hangmani/config/OAuthConst.java
@@ -1,14 +1,16 @@
 package com.project.hangmani.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class OAuthConst {
     public static final String CLIENT_ID = "eec30559b1482179d83dd69beb47a816";
-    private static final String LOGIN_REDIRECT_URI = "http://localhost:8080/user";
+    private static final String LOGIN_REDIRECT_URI = "http://localhost:9090/user";
     public static final String URI = "/oauth/token";
     public static final String USER_INFO_URI = "/v2/user/me";
     public static final String KAUTH_HOST = "kauth.kakao.com";
     public static final String KAPI_HOST = "kapi.kakao.com";
     public static final String UNLINK_URI = "/v1/user/unlink";
-    public static final String POST_REDIRECT_URI = "http://localhost:8080/user";
+    public static final String POST_REDIRECT_URI = "http://localhost:9090/user";
     public static final String GRANT_TYPE = "authorization_code";
     public static final String LOGIN_URL = "/oauth/authorize?client_id="+CLIENT_ID+"&response_type=code&redirect_uri="+LOGIN_REDIRECT_URI+"&scope=";
     public static final String HTTPS = "https://";
