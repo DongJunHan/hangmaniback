cef3924b038d420ecd3475a8354af1cdd7169a61
DongJunHan <gamedokdok@naver.com>
2023-09-22 23:07:56 +0900

    feat: 그 외 에러들을 모두 잡을 수 있도록 Excpetion

--- src/main/java/com/project/hangmani/exception/NetworkExceptionHandler.java
+++ src/main/java/com/project/hangmani/exception/NetworkExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.project.hangmani.exception;
 
+import com.project.hangmani.common.dto.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,5 +24,15 @@ public class NetworkExceptionHandler {
     public String getResponseException(Exception e) {
         log.error("error", e);
         return "error/illegalState";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response exception(Exception e) {
+        return Response.builder()
+                .data(null)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(e.getMessage())
+                .build();
     }
 }
